75065d04e2fb7169b4688018f89b0bb0fdef998f
trung-vt <trung.vuthanh24@gmail.com>
2024-02-25 21:48:02 +0000

    upd Lessons and Hackathon

---
+++ my/Kaggle/ImageWoof/breeds.py
@@ -0,0 +1,12 @@
+DOG_BREEDS = [
+    [0, 'Zuchon', 'https://simple.wikipedia.org/wiki/Zuchon'],
+    [1, 'Rhodesian Ridgeback', 'https://en.wikipedia.org/wiki/Rhodesian_Ridgeback'],
+    [2, 'Beagle', 'https://en.wikipedia.org/wiki/Beagle'],
+    [3, 'American Foxhound', 'https://en.wikipedia.org/wiki/American_Foxhound'],
+    [4, 'Border Terrier', 'https://en.wikipedia.org/wiki/Border_Terrier'],
+    [5, 'Australian Terrier', 'https://en.wikipedia.org/wiki/Australian_Terrier'],
+    [6, 'Golden Retriever', 'https://en.wikipedia.org/wiki/Golden_Retriever'],
+    [7, 'Old English Sheepdog', 'https://en.wikipedia.org/wiki/Old_English_Sheepdog'],
+    [8, 'Samoyed', 'https://en.wikipedia.org/wiki/Samoyed_dog'],
+    [9, 'Carolina Dog', 'https://en.wikipedia.org/wiki/Carolina_Dog'],
+]
